6a78081b616622c86e722c03db50b45ecd52741e
weoz <weoz@ukr.net>
2017-10-07 17:34:29 +0300

    multithreading_and_parallel_computing_in_Java: 007_starting_threads_I_-_runnable

---
+++ multithreading_and_parallel_computing/src/main/java/ua/kruart/basic_multithreading/_02_runnable/App.java
@@ -0,0 +1,29 @@
+package ua.kruart.basic_multithreading._02_runnable;
+
+/**
+ * Created by kruart on 07.10.2017.
+ */
+public class App {
+    public static void main(String[] args) {
+        new Thread(new Runner1()).start();
+        new Thread(new Runner2()).start();
+    }
+}
+
+class Runner1 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Runner1: " + i);
+        }
+    }
+}
+
+class Runner2 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Runner2: " + i);
+        }
+    }
+}
