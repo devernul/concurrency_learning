ebc2a801338ec2abde104d1c036605074578d4bb
weoz <weoz@ukr.net>
2017-10-07 18:33:23 +0300

    multithreading_and_parallel_computing_in_Java: 008_starting_threads_II_-_thread_class

---
+++ multithreading_and_parallel_computing/src/main/java/ua/kruart/basic_multithreading/_03_thread/App.java
@@ -0,0 +1,29 @@
+package ua.kruart.basic_multithreading._03_thread;
+
+/**
+ * Created by kruart on 07.10.2017.
+ */
+public class App {
+    public static void main(String[] args) {
+        new Runner1().start();
+        new Runner2().start();
+    }
+}
+
+class Runner1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Runner1: " + i);
+        }
+    }
+}
+
+class Runner2 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Runner2: " + i);
+        }
+    }
+}
