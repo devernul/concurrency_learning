4d1883842f5661a3f398eeedcf25fb38d68c76a1
weoz <weoz@ukr.net>
2017-10-08 20:30:25 +0300

    multithreading_and_parallel_computing_in_Java: 034_dining_philosopher_problems_V_starting_the_threads

--- multithreading_and_parallel_computing/src/main/java/ua/kruart/_03_dining_philosopher_problems/App.java
+++ multithreading_and_parallel_computing/src/main/java/ua/kruart/_03_dining_philosopher_problems/App.java
@@ -1,7 +1,46 @@
 package ua.kruart._03_dining_philosopher_problems;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by kruart on 08.10.2017.
  */
 public class App {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = null;
+        Philosopher[] philosophers = null;
+
+        try {
+            philosophers = new Philosopher[Constants.NUMBER_OF_PHILOSOPHERS];
+            Chopstick[] chopstick = new Chopstick[Constants.NUMBER_OF_PHILOSOPHERS];
+
+            for (int i = 0; i < Constants.NUMBER_OF_CHOPSTICKS; i++) {
+                chopstick[i] = new Chopstick(i);
+            }
+
+            executorService = Executors.newFixedThreadPool(Constants.NUMBER_OF_PHILOSOPHERS);
+
+            for (int i = 0; i < Constants.NUMBER_OF_PHILOSOPHERS; i++) {
+                philosophers[i] = new Philosopher(i, chopstick[i], chopstick[i + 1  % Constants.NUMBER_OF_CHOPSTICKS]);
+                executorService.execute(philosophers[i]);
+            }
+
+            Thread.sleep(Constants.SIMULATION_RUNNING_TIME);
+
+            for (Philosopher philosopher : philosophers) {
+                philosopher.setFull(true);
+            }
+        } finally {
+            executorService.shutdown();
+
+            while (!executorService.isTerminated()) {
+                Thread.sleep(1000);
+            }
+
+            for (Philosopher p : philosophers) {
+                System.out.println(p + " eats " + p.getEatingCounter());
+            }
+        }
+    }
 }
