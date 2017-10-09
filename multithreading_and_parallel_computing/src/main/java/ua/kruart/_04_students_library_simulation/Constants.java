d7d288c9ea87824e37a42139b21980844a446035
weoz <weoz@ukr.net>
2017-10-09 11:40:24 +0300

    multithreading_and_parallel_computing_in_Java: 037 Student library simulation II constants

---
+++ multithreading_and_parallel_computing/src/main/java/ua/kruart/_04_students_library_simulation/Constants.java
@@ -0,0 +1,9 @@
+package ua.kruart._04_students_library_simulation;
+
+public class Constants {
+
+	private Constants(){}
+	
+	public static final int NUMBER_OF_STUDENTS = 5;
+	public static final int NUMBER_OF_BOOKS = 7;
+}
