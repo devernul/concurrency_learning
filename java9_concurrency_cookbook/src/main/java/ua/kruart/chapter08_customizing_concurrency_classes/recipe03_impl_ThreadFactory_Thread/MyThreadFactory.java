3af4ab774c76becd41939b388e5846f569cc0775
weoz <weoz@ukr.net>
2017-10-24 17:59:47 +0300

    8.3 java9concurrency: Implementing the ThreadFactory interface to generate custom threads

---
+++ java9_concurrency_cookbook/src/main/java/ua/kruart/chapter08_customizing_concurrency_classes/recipe03_impl_ThreadFactory_Thread/MyThreadFactory.java
@@ -0,0 +1,39 @@
+package ua.kruart.chapter08_customizing_concurrency_classes.recipe03_impl_ThreadFactory_Thread;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Factory to create MyThread objects
+ *
+ */
+public class MyThreadFactory implements ThreadFactory {
+
+    /**
+     * Attribute to store the number of threads created in this factory
+     */
+    private AtomicInteger counter;
+
+    /**
+     * String to create the name of the threads created with this factory
+     */
+    private String prefix;
+
+    /**
+     * Constructor of the class. Initialize its parameters
+     * @param prefix First part of the name of the threads created with this factory
+     */
+    public MyThreadFactory (String prefix) {
+        this.prefix = prefix;
+        counter = new AtomicInteger(1);
+    }
+
+    /**
+     * Method that creates a new MyThread thread
+     */
+    @Override
+    public Thread newThread(Runnable r) {
+        return new MyThread(r, prefix + "-" + counter.getAndIncrement());
+    }
+
+}
