a066a1245818b69e36542ce4020446c70222970f
weoz <weoz@ukr.net>
2017-10-25 12:51:56 +0300

    8.7 java9concurrency: Customizing tasks running in the fork/join framework(ForkJoinTask impl)

---
+++ java9_concurrency_cookbook/src/main/java/ua/kruart/chapter08_customizing_concurrency_classes/recipe07_impl_ForkJoinTask/Main.java
@@ -0,0 +1,29 @@
+package ua.kruart.chapter08_customizing_concurrency_classes.recipe07_impl_ForkJoinTask;
+
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * Main class of the example. It creates a ForkJoinPool and a
+ * Task and executes the task in the pool
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+		/* Create an array of 10000 elements*/
+        int array[] = new int[10000];
+
+		/* ForkJoinPool to execute the task*/
+        ForkJoinPool pool = new ForkJoinPool();
+
+		/* Task to increment the elements of the array */
+        Task task = new Task("Task", array, 0, array.length);
+
+		/* Send the task to the pool*/
+        pool.invoke(task);
+
+		/* Shutdown the pool*/
+        pool.shutdown();
+
+		/* Write a message in the console*/
+        System.out.printf("Main: End of the program.\n");
+    }
+}
